d3c404e5a0490268691c6dcb2bcf8f0243051e21
Courser <wangzhongdi@meituan.com>
2018-05-21 19:19:09 +0800

    init demo

---
+++ ShopPhotoDemo/ShopPhotoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShopPhotoDemo
+//
+//  Created by Courser on 2018/5/21.
+//  Copyright © 2018 Courser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
