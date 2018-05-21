ecc09f20a5119471b1062100ee5435b53f20e530
Courser <wangzhongdi@meituan.com>
2018-05-21 20:12:20 +0800

    no message

--- ShopPhotoDemo/ShopPhotoDemo/ViewController.m
+++ ShopPhotoDemo/ShopPhotoDemo/ViewController.m
@@ -8,9 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate,UITableViewDataSource>
 
 @property (nonatomic, strong) UITableView *backgroundView;
+@property (nonatomic, strong) UIView      *headerView;
 
 @end
 
@@ -19,16 +20,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self setupUI];
 }
 
 - (void)setupUI {
     self.backgroundView = [[UITableView alloc] initWithFrame:CGRectZero];
+    self.backgroundView.delegate = self;
+    self.backgroundView.dataSource = self;
+    self.backgroundView.contentInset = UIEdgeInsetsMake(245, 0, 0, 0);
     [self.view addSubview:self.backgroundView];
+    
+    self.headerView = [[UIView alloc] init];
+    self.headerView.backgroundColor = [UIColor blackColor];
+    [self.backgroundView addSubview:self.headerView];
 }
 
 - (void)viewDidLayoutSubviews {
     [super viewDidLayoutSubviews];
     self.backgroundView.frame = self.view.bounds;
+    self.headerView.frame = CGRectMake(0, -50, self.view.bounds.size.width, 50);
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 100;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"reuse"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"reuse"];
+    }
+    cell.contentView.backgroundColor = [UIColor lightGrayColor];
+    return cell;
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    
 }
 
 - (void)didReceiveMemoryWarning {
